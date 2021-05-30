5a6b9e25318519eb0a090526e580bb8ba5eb78b3
Maksymilian Kadukowski <maksymilian.kadukowski@gmail.com>
2021-05-30 20:39:29 +0200

    done

--- src/bazy_danych/forms.py
+++ src/bazy_danych/forms.py
@@ -1,6 +1,7 @@
 from wtforms import StringField, SubmitField, PasswordField
-from wtforms.fields.core import DateTimeField, DecimalField
-from wtforms.validators import DataRequired, NumberRange
+from wtforms.fields.core import BooleanField, DecimalField, FormField, FieldList
+from wtforms.fields.simple import HiddenField
+from wtforms.validators import DataRequired, NumberRange, Regexp
 from flask_wtf import FlaskForm
 
 
@@ -14,3 +15,14 @@ class AddAGradeForm(FlaskForm):
     grade = DecimalField("Grade", validators=[DataRequired(), NumberRange(1, 6)])
     comment = StringField("Comment")
     submit = SubmitField("Add")
+
+
+class AttendanceForm(FlaskForm):
+    lesson_instance = HiddenField(
+        "lesson_instance_id",
+        validators=[DataRequired(), Regexp('^\d+$')])
+
+    attendance = HiddenField(
+        "attendance",
+        validators=[Regexp('^\d*(,\d+)*$')],
+        render_kw={'id': 'attendanceInput'})
